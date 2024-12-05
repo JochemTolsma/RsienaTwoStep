8620adb575a5aab3211a6f9e5bbd1ae82b34c561
JochemTolsma <j.tolsma@ru.nl>
2024-12-05 08:32:17 +0100

    NI statistics

--- R/statistics_beh.R
+++ R/statistics_beh.R
@@ -63,3 +63,61 @@ ts_effFrom <- function(beh, net=NULL, ego, cov) {
 }
 attr(ts_effFrom, "name") <- "effFrom"
 
+
+#' @rdname ts_linear
+#' @export
+#this one calculates the mean opinion push of all connected alters
+#opinion = beh, note that this variable does not automatically falls within [-1,1], thus need to correct for this
+#group = cov
+#H = parameter
+ts_avNiAlt <- function(beh, net, ego, cov=NULL, parameter=0.5,...) {
+  alters <- which(net[ego,]==1)
+  if (length(alters) == 0) {
+    statistic <- 0
+  } else {
+
+    #making sure opinion variable is treated as if within a range of [-1,1]
+    if (!is.null(attributes(beh)$range)) {
+      hrange <- 0.5 * attributes(beh)$range
+    } else {
+      hrange = 1
+    }
+
+    weight_alters <- 1 -  ((abs(beh[ego] - beh[alters])/hrange)*parameter + (1-parameter)*2*abs(cov[ego] != cov[alters]))
+    opinion_delta <- 0.5*((beh[alters] - beh[ego])/hrange)*weight_alters
+    statistic <- 1 - abs(mean(opinion_delta)) #maximize the behavior where the opinion push is minimal
+  }
+  return(statistic)
+}
+attr(ts_avNiAlt, "name") <- "avNiAlt"
+
+#' @rdname ts_linear
+#' @export
+#this one calculates the mean opinion push of one random connected alter
+ts_1NiAlt <- function(beh, net, ego, cov, parameter=0.5, seed = NULL) {
+  alters <- which(net[ego,]==1)
+  if (length(alters) == 0) {
+    statistic <- 0
+  } else {
+
+    #only dyadic influence from a random connected alter, could tweak this weight distance/homophily function
+    if (!is.null(seed)) set.seed(seed)
+    alters <- sample(alters, 1)
+
+    #making sure opinion variable is treated as if within a range of [-1,1]
+    if (!is.null(attributes(beh)$range)) {
+      hrange <- 0.5 * attributes(beh)$range
+    } else {
+      hrange = 1
+    }
+
+    weight_alters <- 1 -  ((abs(beh[ego] - beh[alters])/hrange)*parameter + (1-parameter)*2*abs(cov[ego] != cov[alters]))
+    opinion_delta <- 0.5*((beh[alters] - beh[ego])/hrange)*weight_alters
+    statistic <- 1 - abs(mean(opinion_delta)) #maximize the behavior where the opinion push is minimal
+  }
+  return(statistic)
+}
+attr(ts_1NiAlt, "name") <- "1NiAlt"
+
+
+
